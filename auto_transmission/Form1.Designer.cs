f704dcdaadc81d865b91c0056c47880a753a3cac
AvilovAlex <sanya.a.97@yandex.ru>
2017-12-09 00:36:01 +0300

    Only com Port init

---
+++ auto_transmission/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace auto_transmission
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.arduino = new System.IO.Ports.SerialPort(this.components);
+            this.brakeBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // arduino
+            // 
+            this.arduino.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.arduino_DataReceived);
+            // 
+            // brakeBar
+            // 
+            this.brakeBar.Location = new System.Drawing.Point(72, 125);
+            this.brakeBar.Name = "brakeBar";
+            this.brakeBar.Size = new System.Drawing.Size(100, 23);
+            this.brakeBar.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.brakeBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort arduino;
+        private System.Windows.Forms.ProgressBar brakeBar;
+    }
+}
+
