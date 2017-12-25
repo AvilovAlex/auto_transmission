2d35d24a2dd9fb7fa683e7bd442e7b1d30e05403
Oleg348 <zherdev96@gmail.com>
2017-12-26 00:26:25 +0300

    Update Form1.cs

--- auto_transmission/Form1.cs
+++ auto_transmission/Form1.cs
@@ -166,49 +166,53 @@ namespace auto_transmission
             //  1 -> 2 - 45-50 км/ч
             //  2 -> 3 - 50-60 км/ч
             //  3 -> 4 - 70-75 км/ч
-            switch mode{
-                  case 0:
+            switch (mode) 
+            {
+            		case 0:
                      if (curGear==1){
-                        if ((speeed >=45 && speed <=50) && (ob>=2400 && ob<=2600))
+                        if ((speed >=45 && speed <=50) && (ob>=2400 && ob<=2600))
                            return 2;
                         else
                            return 1;
                      }
 
                      if (curGear==2){
-                        if ((speeed >=50 && speed <=60) && (ob>=2400 && ob<=2600))
+                        if ((speed >=50 && speed <=60) && (ob>=2400 && ob<=2600))
                            return 3;
                         else
                            return 2;
                      }
 
                      if (curGear==3){
-                        if ((speeed >=70 && speed <=75) && (ob>=2400 && ob<=2600))
+                        if ((speed >=70 && speed <=75) && (ob>=2400 && ob<=2600))
                            return 4;
                         else
                            return 3;
                      }
+                     break;
                   case 2:
                      if (curGear==1){
-                        if ((speeed >=45 && speed <=50) && (ob>=2400 && ob<=2600))
+                        if ((speed >=45 && speed <=50) && (ob>=2400 && ob<=2600))
                            return 2;
                         else
                            return 1;
                      }
+                     break;
                   case 3:
                      if (curGear==1){
-                        if ((speeed >=45 && speed <=50) && (ob>=2400 && ob<=2600))
+                        if ((speed >=45 && speed <=50) && (ob>=2400 && ob<=2600))
                            return 2;
                         else
                            return 1;
                      }
 
                      if (curGear==2){
-                        if ((speeed >=50 && speed <=60) && (ob>=2400 && ob<=2600))
+                        if ((speed >=50 && speed <=60) && (ob>=2400 && ob<=2600))
                            return 3;
                         else
                            return 2;
                      }
+                     break;
             }
             return curGear;
         }
